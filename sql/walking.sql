6414c4d5d410615fa676f67e6e46cc3930ce7559
JeongSuIn <71997955+JeongSuIn@users.noreply.github.com>
2021-03-05 14:22:50 +0900

    21.03.05(list 수정)

--- sql/walking.sql
+++ sql/walking.sql
@@ -1,1 +1,10 @@
 SELECT * FROM open.walking;
+
+insert into open.walking(loc_km, about_time, start_point, end_point, walking_time, user_idx) values(1, 2, 'test1', 'test2', 12, 4);
+
+select course_idx, loc_km from open.walking order by course_date desc limit 1; 
+
+select * from open.walking where user_idx=1 ;
+
+select count(*) from open.walking where user_idx=2;
+;
